541f3c11733568ecc922f55604cfccfcff4551cb
Antis <antis28@mail.ru>
2024-06-08 08:33:24 +0300

    add Project(dll) base service chat

---
+++ WcfChat/IServerChatCallBack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WcfChat
+{
+    public interface IServerChatCallBack
+    {
+        [OperationContract]
+        void MsgCallBack();
+    }
+}
---
+++ WcfChat/IServiceChat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfChat
+{
+
+    [ServiceContract(CallbackContract =typeof(IServerChatCallBack))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        int Connect();
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMsg(string msg);
+    }
+}
