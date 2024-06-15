795ad7df43ab7f4dad60e1349ea2eec069d5243b
Antis <antis28@mail.ru>
2024-06-15 23:47:31 +0300

    add mvvm codegenerator

---
+++ ChatClientWPF/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Mvvm.CodeGenerators;
+
+namespace ChatClientWPF
+{
+    [GenerateViewModel]
+    public partial class MainViewModel 
+    {
+        [GenerateProperty]
+        string username;
+        [GenerateProperty]
+        public string ip;
+        [GenerateProperty]
+        string status;
+
+        [GenerateCommand]
+        void Login() => Status = "User: " + Username;
+        bool CanLogin() => !string.IsNullOrEmpty(Username);
+    }
+}
