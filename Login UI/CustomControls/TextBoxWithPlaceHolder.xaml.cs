1b81e193ca61864b43ea8f492f64180300d62946
Antis <antis28@mail.ru>
2024-08-22 20:36:55 +0300

    Переиспользуемый плэйсхолдер

--- Login UI/CustomControls/TextBoxWithPlaceHolder.xaml.cs
+++ Login UI/CustomControls/TextBoxWithPlaceHolder.xaml.cs
@@ -24,5 +24,30 @@ namespace Login_UI.CustomControls
         {
             InitializeComponent();
         }
+
+
+        public string PlaceHolder
+        {
+            get { return (string)GetValue(PlaceHolderProperty); }
+            set { SetValue(PlaceHolderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PlaceHolder.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlaceHolderProperty =
+            DependencyProperty.Register("PlaceHolder", typeof(string), typeof(TextBoxWithPlaceHolder));
+
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(TextBoxWithPlaceHolder));
+
+
     }
 }
