29b62da7f777190ae73d4bcb5ee678b24c117c76
Antis <antis28@mail.ru>
2024-08-25 20:50:28 +0300

    add base title panel for movable

---
+++ WpfChatUI/CustomControls/TopWindowPanel.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+
+namespace WpfChatUI.CustomControls
+{
+    /// <summary>
+    /// Логика взаимодействия для TopWindowPanel.xaml
+    /// </summary>
+    public partial class TopWindowPanel : UserControl
+    {
+        public TopWindowPanel()
+        {
+            InitializeComponent();
+        }
+        MainWindow mainWindow { get => Application.Current.MainWindow as MainWindow; }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                mainWindow.DragMove();
+            }
+        }
+    }
+}
