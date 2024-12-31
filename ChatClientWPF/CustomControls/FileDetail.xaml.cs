f13ce44f0140985ba8e440c2e47a50c1643b237b
Antis <antis28@mail.ru>
2024-12-31 17:46:36 +0300

    add dependency property in control

--- ChatClientWPF/CustomControls/FileDetail.xaml.cs
+++ ChatClientWPF/CustomControls/FileDetail.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,61 @@ namespace ChatClientWPF.CustomControls
         {
             InitializeComponent();
         }
+        
+        #region FileName : string - File name
+        ///<summary>File name</summary>
+        public string FileName
+        {
+            get => (string)GetValue(FileNameProperty);
+            set => SetValue(FileNameProperty, value);
+        }
+        // Using a DependencyProperty as the backing store for FileName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileNameProperty =
+            DependencyProperty.Register(nameof(FileName), typeof(string), typeof(FileDetail));
+        #endregion
+        
+        #region FileSize : string - File size
+        ///<summary>File size</summary>
+        public string FileSize
+        {
+            get => (string)GetValue(FileSizeProperty);
+            set => SetValue(FileSizeProperty, value);
+        }
+        // Using a DependencyProperty as the backing store for FileSize.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileSizeProperty =
+            DependencyProperty.Register(nameof(FileSize), typeof(string), typeof(FileDetail));
+        #endregion
+        
+        #region UploadProgress : int - Upload Progress
+        ///<summary>Upload Progress</summary>
+        public int UploadProgress
+        {
+            get => (int)GetValue(UploadProgressProperty);
+            set => SetValue(UploadProgressProperty, value);
+        }
+        // Using a DependencyProperty as the backing store for UploadProgress.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UploadProgressProperty =
+            DependencyProperty.Register(nameof(UploadProgress), typeof(int), typeof(FileDetail));
+        #endregion
+        
+        #region UploadSpeed : int - Upload speed
+        ///<summary>Upload speed</summary>
+        public int UploadSpeed
+        {
+            get => (int)GetValue(UploadSpeedProperty);
+            set => SetValue(UploadSpeedProperty, value);
+        }
+        // Using a DependencyProperty as the backing store for UploadSpeed.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UploadSpeedProperty =
+            DependencyProperty.Register(nameof(UploadSpeed), typeof(int), typeof(FileDetail));
+        #endregion
+
+
+
+
+
+
+
+
     }
 }
